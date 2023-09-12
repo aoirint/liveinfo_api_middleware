af248ac95a1c82214f443d5eafb22531d373b29c
aoirint <aoirint@gmail.com>
2023-09-12 13:50:38 +0900

    format

--- liveinfo_api_middleware/liveinfo_api.py
+++ liveinfo_api_middleware/liveinfo_api.py
@@ -1,241 +1,276 @@
-from pathlib import Path
-from datetime import datetime
-from zoneinfo import ZoneInfo
-from typing import Optional, Literal
 import json
+from datetime import datetime
+from pathlib import Path
+from typing import Literal, Optional
+from zoneinfo import ZoneInfo
+
 import requests
 from bs4 import BeautifulSoup
 
-
-JST = ZoneInfo('Asia/Tokyo')
+JST = ZoneInfo("Asia/Tokyo")
 
 
 def dump_nicolive_community_live(
-  nicolive_community_id: str,
-  useragent: str,
-  dump_path: Path,
+    nicolive_community_id: str,
+    useragent: str,
+    dump_path: Path,
 ):
-  onair_response = requests.get(
-    f'https://com.nicovideo.jp/api/v1/communities/{nicolive_community_id}/lives/onair.json',
-    headers={
-      'User-Agent': useragent,
-    },
-  )
-  # onair_response.raise_for_status()
-  is_onair = False
-  if onair_response.status_code == 200:
-    onair_data = json.loads(onair_response.text)
-    onair_live = onair_data.get('data', {}).get('live', {})
-
-    is_onair = onair_live.get('status') == 'ON_AIR'
-
-
-  ogp_useragent = f'facebookexternalhit/1.1;Googlebot/2.1;{useragent}'
-
-  watch_response = requests.get(
-    f'https://live.nicovideo.jp/watch/co{nicolive_community_id}',
-    headers={
-      'User-Agent': ogp_useragent,
-    },
-  )
-  watch_response.raise_for_status()
-
-  bs = BeautifulSoup(watch_response.text, 'html5lib')
-  json_ld_tag = bs.find('script', attrs={'type': 'application/ld+json'})
-  json_ld_string = json_ld_tag.string
-
-  json_ld_data = json.loads(json_ld_string)
-  publication = json_ld_data.get('publication', {})
-  author = json_ld_data.get('author', {})
-
-  user_icon_tag = bs.select_one('#watchPage > div.___program-information-area___2mmJb > div.___program-information-header-area___3F--P > div > div.___user-summary___1PSFe.___user-summary___1gieM.user-summary > div.___thumbnail-area___1z7XZ.thumbnail-area > a > img')
-  user_icon_url = user_icon_tag.get('src') if user_icon_tag is not None else None
-
-  community_name_tag = bs.select_one('#watchPage > div.___program-information-area___2mmJb > div.___program-information-body-area___1D8P9 > div.___program-information-side-area___1XQ24 > div > div > div > div > a')
-  community_name = community_name_tag.text if community_name_tag is not None else None
-  community_url = community_name_tag.get('href') if community_name_tag is not None else None
-
-  community_icon_tag = bs.select_one('#watchPage > div.___program-information-area___2mmJb > div.___program-information-body-area___1D8P9 > div.___program-information-side-area___1XQ24 > div > div > div > a > img')
-  community_icon_url = community_icon_tag.get('src') if community_icon_tag is not None else None
-
-  start_time_string = publication.get('startDate')
-  end_time_string = publication.get('endDate')
-
-  og_url_tag = bs.find('meta', attrs={'property': 'og:url'})
-  program_url = og_url_tag.get('content') if og_url_tag is not None else None
-
-  dump_path.parent.mkdir(parents=True, exist_ok=True)
-  dump_path.write_text(
-    json.dumps(
-      {
-        'program': {
-          'title': json_ld_data.get('name'),
-          'description': json_ld_data.get('description'),
-          'url': program_url,
-          'thumbnails': json_ld_data.get('thumbnailUrl'),
-          'startTime': start_time_string,
-          'endTime': end_time_string,
-          'isOnair': is_onair,
+    onair_response = requests.get(
+        f"https://com.nicovideo.jp/api/v1/communities/{nicolive_community_id}/lives/onair.json",
+        headers={
+            "User-Agent": useragent,
         },
-        'community': {
-          'name': community_name,
-          'url': community_url,
-          'iconUrl': community_icon_url,
+    )
+    # onair_response.raise_for_status()
+    is_onair = False
+    if onair_response.status_code == 200:
+        onair_data = json.loads(onair_response.text)
+        onair_live = onair_data.get("data", {}).get("live", {})
+
+        is_onair = onair_live.get("status") == "ON_AIR"
+
+    ogp_useragent = f"facebookexternalhit/1.1;Googlebot/2.1;{useragent}"
+
+    watch_response = requests.get(
+        f"https://live.nicovideo.jp/watch/co{nicolive_community_id}",
+        headers={
+            "User-Agent": ogp_useragent,
         },
-        'user': {
-          'name': author.get('name'),
-          'url': author.get('url'),
-          'iconUrl': user_icon_url,
-        },
-      },
-      ensure_ascii=False,
-    ),
-    encoding='utf-8',
-  )
+    )
+    watch_response.raise_for_status()
+
+    bs = BeautifulSoup(watch_response.text, "html5lib")
+    json_ld_tag = bs.find("script", attrs={"type": "application/ld+json"})
+    json_ld_string = json_ld_tag.string
+
+    json_ld_data = json.loads(json_ld_string)
+    publication = json_ld_data.get("publication", {})
+    author = json_ld_data.get("author", {})
+
+    user_icon_tag = bs.select_one(
+        "#watchPage > div.___program-information-area___2mmJb > div.___program-information-header-area___3F--P > div > div.___user-summary___1PSFe.___user-summary___1gieM.user-summary > div.___thumbnail-area___1z7XZ.thumbnail-area > a > img"
+    )
+    user_icon_url = user_icon_tag.get("src") if user_icon_tag is not None else None
+
+    community_name_tag = bs.select_one(
+        "#watchPage > div.___program-information-area___2mmJb > div.___program-information-body-area___1D8P9 > div.___program-information-side-area___1XQ24 > div > div > div > div > a"
+    )
+    community_name = community_name_tag.text if community_name_tag is not None else None
+    community_url = (
+        community_name_tag.get("href") if community_name_tag is not None else None
+    )
+
+    community_icon_tag = bs.select_one(
+        "#watchPage > div.___program-information-area___2mmJb > div.___program-information-body-area___1D8P9 > div.___program-information-side-area___1XQ24 > div > div > div > a > img"
+    )
+    community_icon_url = (
+        community_icon_tag.get("src") if community_icon_tag is not None else None
+    )
+
+    start_time_string = publication.get("startDate")
+    end_time_string = publication.get("endDate")
+
+    og_url_tag = bs.find("meta", attrs={"property": "og:url"})
+    program_url = og_url_tag.get("content") if og_url_tag is not None else None
+
+    dump_path.parent.mkdir(parents=True, exist_ok=True)
+    dump_path.write_text(
+        json.dumps(
+            {
+                "program": {
+                    "title": json_ld_data.get("name"),
+                    "description": json_ld_data.get("description"),
+                    "url": program_url,
+                    "thumbnails": json_ld_data.get("thumbnailUrl"),
+                    "startTime": start_time_string,
+                    "endTime": end_time_string,
+                    "isOnair": is_onair,
+                },
+                "community": {
+                    "name": community_name,
+                    "url": community_url,
+                    "iconUrl": community_icon_url,
+                },
+                "user": {
+                    "name": author.get("name"),
+                    "url": author.get("url"),
+                    "iconUrl": user_icon_url,
+                },
+            },
+            ensure_ascii=False,
+        ),
+        encoding="utf-8",
+    )
 
 
 def dump_ytlive_channel_live(
-  ytlive_channel_id: str,
-  ytlive_api_key: str,
-  useragent: str,
-  dump_path: Path,
+    ytlive_channel_id: str,
+    ytlive_api_key: str,
+    useragent: str,
+    dump_path: Path,
 ):
-  # チャンネル情報を取得（アイコン）
-  channels_response = requests.get(
-    'https://www.googleapis.com/youtube/v3/channels',
-    params={
-      'key': ytlive_api_key,
-      'part': 'snippet',
-      'id': ytlive_channel_id,
-    },
-    headers={
-      'User-Agent': useragent,
-    },
-  )
-  channels_data = channels_response.json()
-  channel_list_items = channels_data['items']
-  channel = channel_list_items[0]
-
-  channel_snippet_obj = channel.get('snippet')
-  channel_custom_url = channel_snippet_obj.get('customUrl')
-  channel_thumbnails = channel_snippet_obj.get('thumbnails')
-
-
-  # チャンネルの動画リストを取得
-  search_response = requests.get(
-    'https://www.googleapis.com/youtube/v3/search',
-    params={
-      'key': ytlive_api_key,
-      'part': 'id,snippet',
-      'channelId': ytlive_channel_id,
-      'type': 'video',
-      'order': 'date',  # createdAt desc
-      'maxResults': '10',
-    },
-    headers={
-      'User-Agent': useragent,
-    },
-  )
-  search_data = search_response.json()
-  search_list_items = search_data['items']
-
-
-  # 各動画の詳細を取得
-  videos_response = requests.get(
-    'https://www.googleapis.com/youtube/v3/videos',
-    params={
-      'key': ytlive_api_key,
-      'part': 'snippet,status,liveStreamingDetails',
-      'id': ','.join(list(map(lambda item: item['id']['videoId'], search_list_items))),
-    },
-    headers={
-      'User-Agent': useragent,
-    },
-  )
-  videos_data = videos_response.json()
-
-  live_items = []
-  for video_item in videos_data['items']:
-    video_id = video_item['id']
-
-    status_obj = video_item['status']
-
-    privacy_status: Literal['public', 'private', 'unlisted'] = status_obj.get('privacyStatus')
-    if privacy_status != 'public':
-      # 非公開・限定公開のライブ配信・動画は対象にしない
-      continue
-
-    search_item = next(filter(lambda search_item: search_item['id']['videoId'] == video_id, search_list_items))
-
-    live_broadcast_content = search_item['snippet'].get('liveBroadcastContent')
-    live_streming_details_obj = video_item.get('liveStreamingDetails')
-
-    if live_broadcast_content == 'live':
-      # ライブ配信中の番組がある場合、選択する
-      live_items.append(video_item)
-
-    if live_broadcast_content == 'none' and live_streming_details_obj is not None:
-      # 終了済みのライブ番組またはプレミア公開番組がある場合、選択する
-      live_items.append(video_item)
-
-
-  # 最新とその1つ前のライブ番組の順番が交換されるYouTubeの謎仕様の対策（再エンコード処理のせい？）
-  # 実際の放送時間に基づいてソートし直す
-  active_video_item = {}
-  max_start_time = None
-  for video_item in live_items:
-    start_time_string = video_item.get('liveStreamingDetails', {}).get('actualStartTime')
-    start_time = datetime.fromisoformat(start_time_string) if start_time_string is not None else datetime.min
-
-    if max_start_time is None or max_start_time < start_time:
-      active_video_item = video_item
-      max_start_time = start_time
-
-  # Extract data from active_video_item
-  active_video_id = active_video_item.get('id')
-  active_search_item = next(filter(lambda search_item: search_item['id']['videoId'] == active_video_id, search_list_items), {})
-
-  snippet_obj = active_video_item.get('snippet')
-  live_streming_details_obj = active_video_item.get('liveStreamingDetails', {})
-  live_broadcast_content = active_search_item.get('snippet', {}).get('liveBroadcastContent')
-
-  title = snippet_obj.get('title')
-  description = snippet_obj.get('description')
-
-  channel_id = snippet_obj.get('channelId')
-  channel_name = snippet_obj.get('channelTitle')
-
-  thumbnails = snippet_obj.get('thumbnails', {})
-
-  start_time_string = live_streming_details_obj.get('actualStartTime')
-  end_time_string = live_streming_details_obj.get('actualEndTime')
-
-  channel_url = f'https://www.youtube.com/channel/{channel_id}' if channel_id is not None else None
-  if channel_custom_url is not None:
-    channel_url = f'https://www.youtube.com/{channel_custom_url}' # ハンドル名
-
-  dump_path.parent.mkdir(parents=True, exist_ok=True)
-  dump_path.write_text(
-    json.dumps(
-      {
-        'program': {
-          'id': active_video_id,
-          'title': title,
-          'description': description,
-          'url': f'https://www.youtube.com/watch?v={active_video_id}' if active_video_id is not None else None,
-          'thumbnails': thumbnails,
-          'startTime': start_time_string,
-          'endTime': end_time_string,
-          'isOnair': live_broadcast_content == 'live',
+    # チャンネル情報を取得（アイコン）
+    channels_response = requests.get(
+        "https://www.googleapis.com/youtube/v3/channels",
+        params={
+            "key": ytlive_api_key,
+            "part": "snippet",
+            "id": ytlive_channel_id,
         },
-        'channel': {
-          'id': channel_id,
-          'name': channel_name,
-          'url': channel_url,
-          'thumbnails': channel_thumbnails,
+        headers={
+            "User-Agent": useragent,
         },
-      },
-      ensure_ascii=False,
-    ),
-    encoding='utf-8',
-  )
+    )
+    channels_data = channels_response.json()
+    channel_list_items = channels_data["items"]
+    channel = channel_list_items[0]
+
+    channel_snippet_obj = channel.get("snippet")
+    channel_custom_url = channel_snippet_obj.get("customUrl")
+    channel_thumbnails = channel_snippet_obj.get("thumbnails")
+
+    # チャンネルの動画リストを取得
+    search_response = requests.get(
+        "https://www.googleapis.com/youtube/v3/search",
+        params={
+            "key": ytlive_api_key,
+            "part": "id,snippet",
+            "channelId": ytlive_channel_id,
+            "type": "video",
+            "order": "date",  # createdAt desc
+            "maxResults": "10",
+        },
+        headers={
+            "User-Agent": useragent,
+        },
+    )
+    search_data = search_response.json()
+    search_list_items = search_data["items"]
+
+    # 各動画の詳細を取得
+    videos_response = requests.get(
+        "https://www.googleapis.com/youtube/v3/videos",
+        params={
+            "key": ytlive_api_key,
+            "part": "snippet,status,liveStreamingDetails",
+            "id": ",".join(
+                list(map(lambda item: item["id"]["videoId"], search_list_items))
+            ),
+        },
+        headers={
+            "User-Agent": useragent,
+        },
+    )
+    videos_data = videos_response.json()
+
+    live_items = []
+    for video_item in videos_data["items"]:
+        video_id = video_item["id"]
+
+        status_obj = video_item["status"]
+
+        privacy_status: Literal["public", "private", "unlisted"] = status_obj.get(
+            "privacyStatus"
+        )
+        if privacy_status != "public":
+            # 非公開・限定公開のライブ配信・動画は対象にしない
+            continue
+
+        search_item = next(
+            filter(
+                lambda search_item: search_item["id"]["videoId"] == video_id,
+                search_list_items,
+            )
+        )
+
+        live_broadcast_content = search_item["snippet"].get("liveBroadcastContent")
+        live_streming_details_obj = video_item.get("liveStreamingDetails")
+
+        if live_broadcast_content == "live":
+            # ライブ配信中の番組がある場合、選択する
+            live_items.append(video_item)
+
+        if live_broadcast_content == "none" and live_streming_details_obj is not None:
+            # 終了済みのライブ番組またはプレミア公開番組がある場合、選択する
+            live_items.append(video_item)
+
+    # 最新とその1つ前のライブ番組の順番が交換されるYouTubeの謎仕様の対策（再エンコード処理のせい？）
+    # 実際の放送時間に基づいてソートし直す
+    active_video_item = {}
+    max_start_time = None
+    for video_item in live_items:
+        start_time_string = video_item.get("liveStreamingDetails", {}).get(
+            "actualStartTime"
+        )
+        start_time = (
+            datetime.fromisoformat(start_time_string)
+            if start_time_string is not None
+            else datetime.min
+        )
+
+        if max_start_time is None or max_start_time < start_time:
+            active_video_item = video_item
+            max_start_time = start_time
+
+    # Extract data from active_video_item
+    active_video_id = active_video_item.get("id")
+    active_search_item = next(
+        filter(
+            lambda search_item: search_item["id"]["videoId"] == active_video_id,
+            search_list_items,
+        ),
+        {},
+    )
+
+    snippet_obj = active_video_item.get("snippet")
+    live_streming_details_obj = active_video_item.get("liveStreamingDetails", {})
+    live_broadcast_content = active_search_item.get("snippet", {}).get(
+        "liveBroadcastContent"
+    )
+
+    title = snippet_obj.get("title")
+    description = snippet_obj.get("description")
+
+    channel_id = snippet_obj.get("channelId")
+    channel_name = snippet_obj.get("channelTitle")
+
+    thumbnails = snippet_obj.get("thumbnails", {})
+
+    start_time_string = live_streming_details_obj.get("actualStartTime")
+    end_time_string = live_streming_details_obj.get("actualEndTime")
+
+    channel_url = (
+        f"https://www.youtube.com/channel/{channel_id}"
+        if channel_id is not None
+        else None
+    )
+    if channel_custom_url is not None:
+        channel_url = f"https://www.youtube.com/{channel_custom_url}"  # ハンドル名
+
+    dump_path.parent.mkdir(parents=True, exist_ok=True)
+    dump_path.write_text(
+        json.dumps(
+            {
+                "program": {
+                    "id": active_video_id,
+                    "title": title,
+                    "description": description,
+                    "url": f"https://www.youtube.com/watch?v={active_video_id}"
+                    if active_video_id is not None
+                    else None,
+                    "thumbnails": thumbnails,
+                    "startTime": start_time_string,
+                    "endTime": end_time_string,
+                    "isOnair": live_broadcast_content == "live",
+                },
+                "channel": {
+                    "id": channel_id,
+                    "name": channel_name,
+                    "url": channel_url,
+                    "thumbnails": channel_thumbnails,
+                },
+            },
+            ensure_ascii=False,
+        ),
+        encoding="utf-8",
+    )
--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import os
+from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from typing import Optional
-from datetime import datetime, timedelta, timezone
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-
 from liveinfo_api_middleware import liveinfo_api
 
+YTLIVE_CHANNEL_ID = os.environ["YTLIVE_CHANNEL_ID"]
+YTLIVE_API_KEY = os.environ["YTLIVE_API_KEY"]
+YTLIVE_DUMP_PATH = Path(os.environ["YTLIVE_DUMP_PATH"])
 
-YTLIVE_CHANNEL_ID = os.environ['YTLIVE_CHANNEL_ID']
-YTLIVE_API_KEY = os.environ['YTLIVE_API_KEY']
-YTLIVE_DUMP_PATH = Path(os.environ['YTLIVE_DUMP_PATH'])
+NICOLIVE_COMMUNITY_ID = os.environ["NICOLIVE_COMMUNITY_ID"]
+NICOLIVE_DUMP_PATH = Path(os.environ["NICOLIVE_DUMP_PATH"])
 
-NICOLIVE_COMMUNITY_ID = os.environ['NICOLIVE_COMMUNITY_ID']
-NICOLIVE_DUMP_PATH = Path(os.environ['NICOLIVE_DUMP_PATH'])
+CORS_ALLOW_ORIGINS = os.environ["CORS_ALLOW_ORIGINS"].split(",")
 
-CORS_ALLOW_ORIGINS = os.environ['CORS_ALLOW_ORIGINS'].split(',')
-
-USERAGENT = 'aoirint_liveinfo_api_middleware/0.0.0'
+USERAGENT = "aoirint_liveinfo_api_middleware/0.0.0"
 
 app = FastAPI()
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=CORS_ALLOW_ORIGINS,
-  allow_credentials=True,
-  allow_methods=['*'],
-  allow_headers=['*'],
+    CORSMiddleware,
+    allow_origins=CORS_ALLOW_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 nicolive_last_fetched: Optional[datetime] = None
@@ -36,44 +35,59 @@ ytlive_last_fetched: Optional[datetime] = None
 ytlive_interval = timedelta(seconds=60)
 
 
-@app.get('/v1/nicolive')
+@app.get("/v1/nicolive")
 def v1_nicolive():
-  global nicolive_last_fetched
+    global nicolive_last_fetched
 
-  now = datetime.now(tz=timezone.utc)
-  if nicolive_last_fetched is None or nicolive_interval <= now - nicolive_last_fetched:
-    nicolive_last_fetched_string = nicolive_last_fetched.isoformat() if nicolive_last_fetched is not None else 'None'
-    print(f'[{now.isoformat()}] Fetch nicolive (last_fetched_at: {nicolive_last_fetched_string})')
+    now = datetime.now(tz=timezone.utc)
+    if (
+        nicolive_last_fetched is None
+        or nicolive_interval <= now - nicolive_last_fetched
+    ):
+        nicolive_last_fetched_string = (
+            nicolive_last_fetched.isoformat()
+            if nicolive_last_fetched is not None
+            else "None"
+        )
+        print(
+            f"[{now.isoformat()}] Fetch nicolive (last_fetched_at: {nicolive_last_fetched_string})"
+        )
 
-    try:
-      liveinfo_api.dump_nicolive_community_live(
-        nicolive_community_id=NICOLIVE_COMMUNITY_ID,
-        useragent=USERAGENT,
-        dump_path=NICOLIVE_DUMP_PATH,
-      )
-    finally:
-      nicolive_last_fetched = now
+        try:
+            liveinfo_api.dump_nicolive_community_live(
+                nicolive_community_id=NICOLIVE_COMMUNITY_ID,
+                useragent=USERAGENT,
+                dump_path=NICOLIVE_DUMP_PATH,
+            )
+        finally:
+            nicolive_last_fetched = now
 
-  return FileResponse(NICOLIVE_DUMP_PATH)
+    return FileResponse(NICOLIVE_DUMP_PATH)
 
 
-@app.get('/v1/ytlive')
+@app.get("/v1/ytlive")
 def v1_ytlive():
-  global ytlive_last_fetched
+    global ytlive_last_fetched
 
-  now = datetime.now(tz=timezone.utc)
-  if ytlive_last_fetched is None or ytlive_interval <= now - ytlive_last_fetched:
-    ytlive_last_fetched_string = ytlive_last_fetched.isoformat() if ytlive_last_fetched is not None else 'None'
-    print(f'[{now.isoformat()}] Fetch ytlive (last_fetched_at: {ytlive_last_fetched_string})')
+    now = datetime.now(tz=timezone.utc)
+    if ytlive_last_fetched is None or ytlive_interval <= now - ytlive_last_fetched:
+        ytlive_last_fetched_string = (
+            ytlive_last_fetched.isoformat()
+            if ytlive_last_fetched is not None
+            else "None"
+        )
+        print(
+            f"[{now.isoformat()}] Fetch ytlive (last_fetched_at: {ytlive_last_fetched_string})"
+        )
 
-    try:
-      liveinfo_api.dump_ytlive_channel_live(
-        ytlive_channel_id=YTLIVE_CHANNEL_ID,
-        ytlive_api_key=YTLIVE_API_KEY,
-        useragent=USERAGENT,
-        dump_path=YTLIVE_DUMP_PATH,
-      )
-    finally:
-      ytlive_last_fetched = now
+        try:
+            liveinfo_api.dump_ytlive_channel_live(
+                ytlive_channel_id=YTLIVE_CHANNEL_ID,
+                ytlive_api_key=YTLIVE_API_KEY,
+                useragent=USERAGENT,
+                dump_path=YTLIVE_DUMP_PATH,
+            )
+        finally:
+            ytlive_last_fetched = now
 
-  return FileResponse(YTLIVE_DUMP_PATH)
+    return FileResponse(YTLIVE_DUMP_PATH)
